a4fc824ebe47b2522a5a3c9170f8a04642f8bfca
Gazorby <gazorby@pm.me>
2020-04-15 14:59:05 +0200

    🐛 fix: remove .git-remotes when uninstall

--- conf.d/check_git_id.fish
+++ conf.d/check_git_id.fish
@@ -2,3 +2,7 @@ if not test -f "$HOME"/.git-remotes
     touch "$HOME"/.git-remotes
     echo "Fill the "$HOME"/.git-remotes" file!
 end
+
+function __check_git_id_uninstall --on-event check_git_id_uninstall
+    rm "$HOME"/.git-remotes
+end
