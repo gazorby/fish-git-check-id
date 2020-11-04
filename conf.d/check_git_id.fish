f900610ecbc92e81f378e2283331135806491015
Gazorby <gazorby@pm.me>
2020-11-04 17:57:35 +0100

    ♻️ refactor: use fisher events

--- conf.d/check_git_id.fish
+++ conf.d/check_git_id.fish
@@ -1,6 +1,8 @@
-if not test -f "$HOME"/.git-remotes
-    touch "$HOME"/.git-remotes
-    echo "Fill the "$HOME"/.git-remotes" file!
+function __check_git_id_install --on-event check_git_id_install
+    if not test -f "$HOME"/.git-remotes
+        touch "$HOME"/.git-remotes
+        echo "Fill the "$HOME"/.git-remotes" file!
+    end
 end
 
 function __check_git_id_uninstall --on-event check_git_id_uninstall
